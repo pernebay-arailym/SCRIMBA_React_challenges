4857b92eb839e504997354685ac27e8b3b074d18
pernebay-arailym <pernebayarailym@gmail.com>
2025-04-25 16:13:48 +0200

    upvotes, comments, isPun, console work

--- custom_components/src/props_practice/App.jsx
+++ custom_components/src/props_practice/App.jsx
@@ -16,9 +16,41 @@
  * If you don't pass in a "question" prop, how might you make it only 
  * show the punchline?
  */
+import Joke from "./Joke"
+import "./index.css"
 
 export default function App() {
     return (
-        <></>
+        <main>
+            <Joke 
+                punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+                upvotes={10}
+                isPun={true}
+                comments={[
+                    {author: "", text: "", title: ""},
+                    {author: "", text: "", title: ""}
+                ]}
+            />
+            <Joke
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomware!"
+                isPun={true}
+            />
+            <Joke
+                setup="Why don't pirates travel on mountain roads?"
+                punchline="Scurvy."  
+                isPun={true}                       
+            />
+            <Joke 
+                setup="Why do bees stay in the hive in the winter?"
+                punchline="Swarm."
+                isPun={true}
+            />
+            <Joke
+                setup="What's the best thing about Switzerland?"
+                punchline="I don't know, but the flag is a big plus!"
+                isPun={false}
+            />
+        </main>
     )
 }
