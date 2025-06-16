b251e46bfe5764ad0d19b3b7bc8b64a113e72eb0
pernebay-arailym <pernebayarailym@gmail.com>
2025-06-16 10:41:09 +0200

    imported the data

--- custom_components/src/Travel_Journal_PJ/App.jsx
+++ custom_components/src/Travel_Journal_PJ/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import data from "./data"
+
 
 /**
  * Challenge:
@@ -18,7 +20,7 @@ export default function App() {
                 <Entry 
                     img={{ src: "/rabat.png", alt: "rabat lighthouse"}}
                     title="Rabat Lighthouse"
-                    country="Morocco"
+                    country="Morocco, Rabat"
                     googleMapsLink="https://www.google.com/maps/place/%D0%9C%D0%B0%D1%8F%D0%BA+%D0%A0%D0%B0%D0%B1%D0%B0%D1%82%D0%B0/@34.0315238,-6.8469585,17z/data=!3m1!4b1!4m6!3m5!1s0xda76c0d1490f88d:0xd9f1e855759d02e7!8m2!3d34.0315238!4d-6.8443782!16s%2Fg%2F1237d80x?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D"
                     dates="26 March, 2025 - 27 March, 2025" 
                     text="Standing at the edge of the Atlantic Ocean, the Rabat Lighthouse is an enduring symbol of Morocco’s maritime heritage."
--- custom_components/src/Travel_Journal_PJ/data.js
+++ custom_components/src/Travel_Journal_PJ/data.js
@@ -6,7 +6,7 @@ export default [
             alt: "rabat lighthouse"
         },
         title: "Rabat Lighthouse",
-        country: "Morocco",
+        country: "Morocco, Rabat",
         googleMapsLink: "https://www.google.com/maps/place/%D0%9C%D0%B0%D1%8F%D0%BA+%D0%A0%D0%B0%D0%B1%D0%B0%D1%82%D0%B0/@34.0315238,-6.8469585,17z/data=!3m1!4b1!4m6!3m5!1s0xda76c0d1490f88d:0xd9f1e855759d02e7!8m2!3d34.0315238!4d-6.8443782!16s%2Fg%2F1237d80x?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D",
         dates: "26 March, 2025 - 27 March, 2025",
         text: "Standing at the edge of the Atlantic Ocean, the Rabat Lighthouse is an enduring symbol of Morocco’s maritime heritage."
@@ -14,25 +14,25 @@ export default [
     {
         id: 2,
         img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
-            alt: "Sydney Opera House"
+            src: "casablanca.png",
+            alt: "Hassan II Mosque"
         },
-        title: "Sydney Opera House",
-        country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
-        dates: "27 May, 2021 - 8 Jun, 2021",
-        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+        title: "Hassan II Mosque",
+        country: "Morocco, Casablanca",
+        googleMapsLink: "https://maps.app.goo.gl/BbhgfSzRn8UrMLX78",
+        dates: "28 March, 2025 - 29 March, 2025",
+        text: "The Hassan II Mosque is the second largest functioning mosque in Africa and is the 14th largest in the world. Its minaret is the world's second tallest minaret at 210 metres."
     },
     {
         id: 3,
         img: {
             src: "https://scrimba.com/links/travel-journal-norway-image-url",
-            alt: "Geirangerfjord"
+            alt: "lalla takerkoust"
         },
-        title: "Geirangerfjord",
-        country: "Norway",
-        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+        title: "Lalla Takerkoust",
+        country: "Morocco, Marrakech",
+        googleMapsLink: "https://maps.app.goo.gl/4rDRBD53NbAVTDGi9",
+        dates: "30 March, 2025 - 31 March, 2025",
+        text: "In recent years, Lalla Takerkoust has become a popular tourist destination. There are a number of hotels and resorts located around the lake, catering to visitors who come to enjoy the area's recreational opportunities. In addition to swimming and boating, visitors can also go hiking in the surrounding forests and fields. "
     },
 ]
