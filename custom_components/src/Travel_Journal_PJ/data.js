d5e5a8fee1954373f41fa619de1aa67de2e5efb2
pernebay-arailym <pernebayarailym@gmail.com>
2025-06-16 11:20:51 +0200

    displayed the array of Entry components in place of the current  *   hard-coded <Entry /> instance

--- custom_components/src/Travel_Journal_PJ/data.js
+++ custom_components/src/Travel_Journal_PJ/data.js
@@ -9,7 +9,7 @@ export default [
         country: "Morocco, Rabat",
         googleMapsLink: "https://www.google.com/maps/place/%D0%9C%D0%B0%D1%8F%D0%BA+%D0%A0%D0%B0%D0%B1%D0%B0%D1%82%D0%B0/@34.0315238,-6.8469585,17z/data=!3m1!4b1!4m6!3m5!1s0xda76c0d1490f88d:0xd9f1e855759d02e7!8m2!3d34.0315238!4d-6.8443782!16s%2Fg%2F1237d80x?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D",
         dates: "26 March, 2025 - 27 March, 2025",
-        text: "Standing at the edge of the Atlantic Ocean, the Rabat Lighthouse is an enduring symbol of Morocco’s maritime heritage."
+        text: "The Phare de Rabat is a lighthouse in Rabat, Morocco. Standing close to the Atlantic Ocean, it helps guide ships safely. It's unique for its blend of traditional and modern design elements. Built in 1920, the lighthouse has been an essential part of Rabat's maritime history, contributing to its coastal safety."
     },
     {
         id: 2,
@@ -21,7 +21,7 @@ export default [
         country: "Morocco, Casablanca",
         googleMapsLink: "https://maps.app.goo.gl/BbhgfSzRn8UrMLX78",
         dates: "28 March, 2025 - 29 March, 2025",
-        text: "The Hassan II Mosque is the second largest functioning mosque in Africa and is the 14th largest in the world. Its minaret is the world's second tallest minaret at 210 metres."
+        text: "The Hassan II Mosque is the second largest functioning mosque in Africa and is the 14th largest in the world. Its minaret is the world's second tallest minaret at 210 metres.The minaret is 60 stories high topped by a laser, the light from which is directed towards Mecca.The mosque stands on a promontory looking out to the Atlantic Ocean."
     },
     {
         id: 3,
