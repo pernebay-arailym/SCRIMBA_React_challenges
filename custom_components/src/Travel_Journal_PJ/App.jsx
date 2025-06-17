6f562cf5381e47ee516a4c9072cc699996c8eeec
pernebay-arailym <pernebayarailym@gmail.com>
2025-06-17 11:29:41 +0200

    key props, entry props

--- custom_components/src/Travel_Journal_PJ/App.jsx
+++ custom_components/src/Travel_Journal_PJ/App.jsx
@@ -17,12 +17,8 @@ export default function App() {
     const entryElements = data.map((entry) => {
         return (
             <Entry 
-                img={{ src: entry.img.src, alt: entry.img.alt }}
-                title={entry.title}
-                country={entry.country}
-                googleMapsLink={entry.googleMapsLink}
-                dates={entry.dates}
-                text={entry.text}
+                key={entry.id}
+                entry={entry}
               />
         )
     })
--- custom_components/src/Travel_Journal_PJ/components/Entry.jsx
+++ custom_components/src/Travel_Journal_PJ/components/Entry.jsx
@@ -15,23 +15,22 @@
  */
 
 export default function Entry(props) {
-    console.log(props)
     return (
         <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image" 
-                    src={props.img.src}
-                    alt={"props.img.alt"}
+                    src={props.entry.img.src}
+                    alt={props.entry.img.alt}
                 />
             </div>
             <div className="info-container">
                 <img className="marker"src="/marker.png" alt="map marker icon"/>
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}>View on Google Maps</a>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink}>View on Google Maps</a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
             </div>
         </article>
     )
